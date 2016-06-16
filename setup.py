c1dd0e6981554f39f0c7592602986aefba873e23
Peter Schulam <pschulam@gmail.com>
2016-06-16 14:51:04 -0400

    Include module in setup.

--- setup.py
+++ setup.py
@@ -2,4 +2,5 @@ from distutils.core import setup
 
 setup(name='lmbases', version='1.0.0',
       author='Peter Schulam',
-      author_email='pschulam@gmail.com')
+      author_email='pschulam@gmail.com',
+      py_modules=['lmbases'])
