b95e5041e415c61623baf624a9cbc23ae9f46508
Peter Schulam <pschulam@gmail.com>
2016-06-27 12:53:38 -0400

    Release v1.0.4.

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 
-setup(name='lmbases', version='1.0.2',
+setup(name='lmbases', version='1.0.4',
       author='Peter Schulam',
       author_email='pschulam@gmail.com',
       py_modules=['lmbases'])
