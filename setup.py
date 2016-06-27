8e2e549056574b03c1eb393ecbb35e51fbb60375
Peter Schulam <pschulam@gmail.com>
2016-06-27 12:51:33 -0400

    Fix version in setup.

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 
-setup(name='lmbases', version='1.0.0',
+setup(name='lmbases', version='1.0.2',
       author='Peter Schulam',
       author_email='pschulam@gmail.com',
       py_modules=['lmbases'])
